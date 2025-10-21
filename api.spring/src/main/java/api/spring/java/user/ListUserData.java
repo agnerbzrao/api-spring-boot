4c6fc02eb5f57e1d384a5b38732831fad67e1f5e
Agner Bezerra <agner.bezerra@hotmail.com>
2025-10-21 10:42:55 -0300

    adjusts the constructor indentation

--- api.spring/src/main/java/api/spring/java/user/ListUserData.java
+++ api.spring/src/main/java/api/spring/java/user/ListUserData.java
@@ -2,8 +2,14 @@ package api.spring.java.user;
 
 import api.spring.java.entity.UserEntity;
 
-public record ListUserData(String firstName, String email) {
+public record ListUserData(Long id, String firstName, String email, Boolean actived) {
     public ListUserData(UserEntity userEntity){
-        this(userEntity.getFirstName(), userEntity.getEmail());
+
+        this(
+            userEntity.getId(),
+            userEntity.getFirstName(),
+            userEntity.getEmail(),
+            userEntity.getActived()
+        );
     }
 }
