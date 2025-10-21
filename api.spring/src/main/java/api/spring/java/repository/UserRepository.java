0aa9f5b58427bbb9eed141c45c19231c30914875
Agner Bezerra <agner.bezerra@hotmail.com>
2025-10-21 10:39:37 -0300

    Added the findAllByActivedTrue method.

--- api.spring/src/main/java/api/spring/java/repository/UserRepository.java
+++ api.spring/src/main/java/api/spring/java/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package api.spring.java.repository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import api.spring.java.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Page<UserEntity> findAllByActivedTrue(Pageable pagination);
 }
